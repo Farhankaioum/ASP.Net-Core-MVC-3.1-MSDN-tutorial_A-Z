f0b99409aebc71bc4e91deaf7913d82fa2ab61cd
FarhanKaioum <km1472233@gmail.com>
2020-01-28 21:55:04 +0600

    Simple model CRUD

---
+++ MSDocumentation_Asp_NetCore/Data_Access_MVC/ContosoUniversity/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ContosoUniversity.Models;
+using ContosoUniversity.Data;
+using ContosoUniversity.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SchoolContext _context;
+
+        public HomeController(ILogger<HomeController> logger, SchoolContext context)
+        {
+            _logger = logger;
+            this._context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // about page for how many student and enrollments
+        public async Task<ActionResult> About()
+        {
+            var data = from student in _context.Students
+                       group student by student.EnrollmentDate into dataGroup
+                       select new EnrollmentDateGroupViewModel
+                       {
+                           EnrollmentDate = dataGroup.Key,
+                           StudentCount = dataGroup.Count()
+                       };
+
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MSDocumentation_Asp_NetCore/Data_Access_MVC/ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,30 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
